7aa0a38a46f460276dc6ce1602af79f1f915df34
spitchell <mr.spitchell@gmail.com>
2018-02-27 16:46:56 +0000

    puerile examples

--- App.js
+++ App.js
@@ -1,42 +1,67 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Picker } from 'react-native';
 
 
 
 export default class App extends React.Component {
     constructor(){
         super();
-        this.state = {visible: true};
+        this.state = {
+            visible: true,
+            term: "none chosen"
+        };
     }
 
     toggleVisibility=()=> {
         this.setState(prevState => {
             return {visible: !prevState.visible}
-        });
+        })
     }
 
     render() {
     let pic = {uri: 'http://finedininglovers.cdn.crosscast-system.com/BlogPost/l_12787_rainbow-corn-.jpg'};
         return (
             <View style={styles.container}>
-                {this.state.visible ? 
+                {this.state.visible ?
                     <Image source={pic} style={{width: 300, height: 210}}/>
                     :
                     null}
-                <Button 
+                <Button
                     title="toggle image"
                     onPress={this.toggleVisibility}
                 />
+                <Picker
+                    style={{width: 300}}
+                    selectedValue={this.state.term}
+                    onValueChange={(itemValue, itemIndex) => this.setState({term: itemValue})}
+                    prompt="Words for Bum"
+                    selected="Pick a Bum Word"
+                >
+                        <Picker.Item label="Pick a Bum Word" />
+                        <Picker.Item label="Anus" value="dry" />
+                        <Picker.Item label="Sphincter" value="medical" />
+                        <Picker.Item label="Rusty Drain" value="made up" />
+                        <Picker.Item label="Chocolate Starfish" value="comedy" />
+                        <Picker.Item label="Ringpiece" value="vernacular" />
+                </Picker>
+                <Text style={styles.header}>{this.state.term}</Text>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    header: {
+        fontSize: 40,
+        color: 'red'
+    }
+
+
+
 });
